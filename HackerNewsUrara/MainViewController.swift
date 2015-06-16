09cc871280e691f8d53fc244868d9f55e528f773
Urara Taguchi <urara.rarara.0312@gmail.com>
2015-06-16 17:48:00 +0900

    HackerNews

--- HackerNewsUrara/MainViewController.swift
+++ HackerNewsUrara/MainViewController.swift
@@ -2,47 +2,47 @@
 //  MainViewController.swift
 //  HackerNewsUrara
 //
-//  Created by 田口うらら on 2015/06/01.
+//  Created by 田口うらら on 2015/06/16.
 //  Copyright (c) 2015年 田口うらら. All rights reserved.
 //
 
 import UIKit
 
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-    let PostCellidentifer = "PostCell"
+    
+    let PostCellIdentifer = "PostCell"
     let ShowBrowserIdentifer = "ShowBrowser"
     let PullToRefreshString = "Pull to Refresh"
-    let ReadTextColor = UIColor(red: 0.467, green: 0.467, blue:0.467, alpha:1.0)
-    let ReadDetailTextColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1.0)
-    let FetchErrorMessage = "Could not Fetch Post"
-    let NoPostsErrorMessage = "No more Posts to Fetch"
+    let ReadTextColor = UIColor(red: 0.467, green: 0.467, blue: 0.467, alpha: 1.0)
+    let ReadDetailTextColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha:1.0)
+    let FetchErrorMessage = "Could Not Fetch Posts"
+    let NoPostsErrorMessage = "No More Posts to Fetch"
     let ErrorMessageLabelTextColor = UIColor.grayColor()
     let ErrorMessageFontSize: CGFloat = 16
     let DefaultPostFilterType = PostFilterType.Top
     
-    var post Filter: PostFilterType!
+    var postFilter: PostFilterType!
     var posts: [HNPost]!
     var nextPageId: String!
-    var scrolledToBottom: Bool!
-    var refreshControl: UIRefreshControl!
+    var scrolledToBottom: BOol!
+    var refreshControll: UIRefreshControl!
     var errorMessageLabel: UILabel!
     
     @IBOutlet weak var tableView: UITableView!
     
-    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         postFilter = DefaultPostFilterType
-        posts = []
+        post = []
         nextPageId = ""
-        scrolledToBottom = UIRefreshControl()
+        scrolledToBottom = false
+        refreshControl = UIRefreshControl()
     }
     
-    override func viewDidLoad() {
+    override func viewDidLoad(){
         super.viewDidLoad()
-        
+        configureUI()
+        fetchPosts()
     }
-
+    
 }
-
